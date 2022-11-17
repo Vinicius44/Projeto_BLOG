e91d81d67db6b4ea7213f5db7b2e1bd3619dcb41
Vinicius da Silva Nogueira <msn-dovinicius@hotmail.com>
2022-11-17 17:26:35 -0300

    Arquivo de conexão com Mysql

--- config.php
+++ config.php
@@ -12,6 +12,11 @@ $autoload = function($class){
 
 	define("INCLUDE_PATH", "http://localhost/Portf%c3%b3lio/Back-End/Projeto_Noticia/");
 
+	define("HOST", "localhost");
+	define("USER", "root");
+	define("PASSWORD", "");
+	define("DATABASE" , "projeto_blog");
+
 
 
 ?>
--- pages/painel/cadastrar_categorias.php
+++ pages/painel/cadastrar_categorias.php
@@ -1,9 +1,30 @@
 
 
 <div class="container_Cadastrar_Categ">
-	
-		<label>Nome da Categoria:</label>
-		<input class="w100" type="text" name="cadastrar_cat" >
-		<input type="submit" name="acao" value="Cadastrar Categoria">
+		
+		<form method="post">
+			<label>Nome da Categoria:</label>
+			<input class="w100" type="text" name="cadastrar_cat" >
+			<input type="submit" name="acao" value="Cadastrar Categoria" required>
+
+			<input class="w100" type="hidden" name="tabela" value="tb_site.categorias">
+
+		</form>
+
+
+		<?php 
+
+			if(isset($_POST['acao'])){
+
+
+				$sql = Painel::insert($_POST);
+
+				if($sql == true){
+					echo "A categoria foi cadastrada";
+				}
+
+			}
+
+		?>
 
 </div>
